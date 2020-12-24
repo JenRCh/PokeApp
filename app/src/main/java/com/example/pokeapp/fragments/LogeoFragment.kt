b58a6f45202809ed28f962fbce0a50a6b202d410
Jennilyn <jennilyn@fitprotracker.com>
2020-12-23 19:53:08 -0600

    Changes

--- app/src/main/java/com/example/pokeapp/fragments/LogeoFragment.kt
+++ app/src/main/java/com/example/pokeapp/fragments/LogeoFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.text.Editable
 import android.text.TextUtils
 import android.text.TextWatcher
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -11,9 +12,21 @@ import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.example.pokeapp.R
 import com.example.pokeapp.models.User
+import com.jakewharton.rxbinding4.view.clicks
+import com.jakewharton.rxbinding4.widget.textChanges
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.core.Flowable.combineLatest
+import io.reactivex.rxjava3.core.Observable.combineLatest
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.kotlin.Flowables.combineLatest
+import io.reactivex.rxjava3.kotlin.Observables.combineLatest
 import kotlinx.android.synthetic.main.fragment_logeo.*
+import java.util.*
+import java.util.concurrent.TimeUnit
 
 class LogeoFragment : Fragment() {
+
+    private val disposables = CompositeDisposable()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,11 +36,18 @@ class LogeoFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_logeo, container, false)
     }
 
+    override fun onDestroyView() {
+        disposables.clear()
+        super.onDestroyView()
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        btnLogin.setOnClickListener{
-            val action = LogeoFragmentDirections.actionLogeoFragment2ToHomeFragment2(User(entrenador.text.toString(),rMasculino.isChecked,rFemenino.isChecked))
+
+       btnLogin.setOnClickListener{
+           val action = LogeoFragmentDirections.actionLogeoFragment2ToHomeFragment2(
+                   User(entrenador.text.toString(),rMasculino.isChecked,rFemenino.isChecked))
             findNavController().navigate(action)
            // findNavController().navigate(R.id.action_logeoFragment2_to_homeFragment2)
         }
@@ -35,15 +55,15 @@ class LogeoFragment : Fragment() {
         entrenador.addTextChangedListener(object : TextWatcher{
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
 
-            }
+          }
 
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+           override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
 
-            }
+           }
 
-            override fun afterTextChanged(s: Editable?) {
+           override fun afterTextChanged(s: Editable?) {
                val areValidFields : Boolean = !TextUtils.isEmpty(entrenador.text.toString())
-                btnLogin.isEnabled = areValidFields
+               btnLogin.isEnabled = areValidFields
 
                 entrenador.error = if(TextUtils.isEmpty(entrenador.text.toString())) "Campo requerido" else null
             }

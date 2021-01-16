cdff543f195f2632de3ee87bfcfc93241fae6bd0
Jennilyn <jennilyn@fitprotracker.com>
2021-01-16 08:52:30 -0600

    Changes

--- app/src/main/java/com/example/pokeapp/db/PokemonDAO.kt
+++ app/src/main/java/com/example/pokeapp/db/PokemonDAO.kt
@@ -2,12 +2,13 @@ package com.example.pokeapp.db
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface PokemonDAO {
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertFavorite(pokemondb : PokemonDB)
 
     @Query("SELECT * FROM pokemondb")
